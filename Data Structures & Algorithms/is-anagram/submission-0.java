8c6e63252fc6e0a69eb76178f50a5835ec884f75
ji2nook <95744919+ji2nook@users.noreply.github.com>
2026-07-07 11:07:15 +0900

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        char[] arr_s = s.toCharArray();
+        char[] arr_t = t.toCharArray();
+
+        Arrays.sort(arr_s);
+        Arrays.sort(arr_t);
+
+        return Arrays.equals(arr_s, arr_t);
+
+    }
+}
